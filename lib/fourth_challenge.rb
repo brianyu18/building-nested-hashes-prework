10dc959b965f6de16188fe95be9d108d6328797b
brianyu18 <brianyu1990@gmail.com>
2018-09-27 04:18:19 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -21,7 +21,7 @@ def fourth_challenge
       :heroine_friends => [
         {:name => "Steven",
         :age => "30",
-        :attitude => "confuse"
+        :attitude => "confused"
         },
         {:name => "Nurse",
         :age => "44",
